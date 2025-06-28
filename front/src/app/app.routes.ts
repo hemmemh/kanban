1d8fa49a5cceb5a7bf5f8251ab13e37b93b9a91d
tagir <makhmutov_tagir@bk.ru>
2025-06-28 08:41:28 +0500

    chore: base angular structure and board route

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',
+        loadComponent:() => import('./features/board/pages/board-page/board-page').then(m => m.BoardPage)
+    },
+    {
+     path: '**',
+     redirectTo: '',
+    },
+];
